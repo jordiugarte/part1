182fa77efe48d0b2c25791859271a7e4a585735a
jordiugarte <48989035+jordiugarte@users.noreply.github.com>
2021-03-12 08:46:42 -0400

    Recuperatorio

--- src/test/java/configuration/Config.java
+++ src/test/java/configuration/Config.java
@@ -1,9 +1,7 @@
 package configuration;
 
 public class Config {
-
     public static final String AUTH_BASIC = "Authorization";
-    public static final String AUTH_BASIC_VALUE = "Basic e096ad9fb6c54a5ab84819b1edd9b863";
+    public static final String AUTH_BASIC_VALUE = "Basic am9yZGlAdWdhcnRlLmNvbToxMjM0NTY3ODk=";
     public static final String TOKEN = "Token";
-
 }
